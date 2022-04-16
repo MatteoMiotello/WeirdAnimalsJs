0f994f9fc24aca93aac1fdc08611f593244493bc
matteo_miotello <matteo.miotello@shellrent.com>
2022-04-16 14:30:18 +0200

    fixed

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
-export = generateWeirdAnimal;
-
-declare function generateWeirdAnimal( language: WeirdAnimals.LanguageSet ): string;
+export = WeirdAnimals;
 
 declare namespace WeirdAnimals {
     export enum LanguageSet {
         IT = 'it-IT',
         EN = 'en-EN',
     }
+
+    export function generateWeirdAnimal( language: WeirdAnimals.LanguageSet ): string;
 }
