a4f0583abd7ffab9db3d1929b4fcb1fc37e9eeec
matteo_miotello <matteo.miotello@shellrent.com>
2022-04-25 20:27:01 +0200

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # WeirdAnimalsJs
 Javascript framework that generates random animals with an adjective
+
+# Usage
+
+```
+const weirdanimalsjs = require("weirdanimalsjs")
+
+console.log( weirdanimalsjs.generateWeirdAnimal( weirdanimalsjs.LanguageSet.EN ) )
+```
+
+## Available languages
+
+- `LanguageSet.IT` (it-IT) Italiano
+- `LanguageSet.EN` (en-EN) English
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function randomNum(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 function generateWeirdAnimal( languageSet ) {
+    if ( !languageSet  )
+
     const Language = require( './languages/' + languageSet );
     const languages = new Language();
     return languages.firstWord[ randomNum( 0, languages.firstWord.length ) ] + ' ' + languages.secondWord[ randomNum( 0, languages.secondWord.length ) ];
